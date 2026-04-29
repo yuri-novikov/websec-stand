4edb0f72abfc956f3ed26622dade5e4af5194601
Yuri Novikov <yuriv@freakingloud.net>
2026-04-29 10:11:12 +0300

    split two pm2 apps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: "websec-stand-dev",
-      cwd: "/var/www/websec-stand",
+      name: "websec-stand-frontend",
+      cwd: "/var/www/websec-stand/frontend",
+      script: "yarn",
+      args: "dev",
+      interpreter: "none",
+      autorestart: true,
+      watch: false,
+      max_restarts: 10,
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "websec-stand-backend",
+      cwd: "/var/www/websec-stand/backend",
       script: "yarn",
       args: "dev",
       interpreter: "none",
